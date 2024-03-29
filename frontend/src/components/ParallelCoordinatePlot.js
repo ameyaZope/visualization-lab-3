d31d7844e60d8d94d5fd488bd1560e4375bedae1
ameyaZope <zopeameya@gmail.com>
2024-03-28 23:21:39 -0400

    Correcting the margin of PCP

--- frontend/src/components/ParallelCoordinatePlot.js
+++ frontend/src/components/ParallelCoordinatePlot.js
@@ -7,7 +7,7 @@ function ParallelCoordinatePlot({ numClusters, chosenDimensions }) {
 
 
 	useEffect(() => {
-		const margin = { top: 80, right: 10, bottom: 50, left: 50 },
+		const margin = { top: 80, right: 50, bottom: 50, left: 50 },
 			width = 1000 - margin.left - margin.right,
 			height = 300 - margin.top - margin.bottom;
 
